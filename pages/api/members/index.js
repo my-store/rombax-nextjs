aa1d796912d8761d65e906c3aa9791103c7a5cfe
Izzat Alharis <izzatalharist@gmail.com>
2021-08-10 20:40:57 +0700

    Update api/members controller

--- pages/api/members/index.js
+++ pages/api/members/index.js
@@ -4,24 +4,11 @@ const DB = new AsyncNedb({filename: "databases/Members.db", autoload: true})
 async function handler(req, res)
 {
   if(req.method === "GET") {
-    try {
-      const members = await DB.asyncFind({}, [['limit', 100]])
-      if(members.length > 0) {
-        res.status(200).json({
-          data: members,
-          message: `${members.length} data fetched.`
-        })
-      }
-      else {
-        res.status(200).json({
-          data: null,
-          message: "Data masih kosong"
-        })
-      }
-    }
-    catch(error) {
-      res.status(error).json(error)
-    }
+    const members = await DB.asyncFind({}, [['limit', 100]])
+    res.status(200).json({
+      data: members,
+      message: `${members.length} data fetched.`
+    })
   }
   else if(req.method === "POST") {
     // 
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,4 +1,7 @@
-export default function handler(req, res)
+const { AsyncNedb } = require("nedb-async")
+const DB = new AsyncNedb({filename: "databases/Members.db", autoload: true})
+
+export default async function handler(req, res)
 {
   if(req.method === "GET") {
       res.status(200).json({
